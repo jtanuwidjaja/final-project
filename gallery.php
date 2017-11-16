3b1532394e683bff62f2a4503e3b9c423527dc77
Konstantin <hvankostya@gmail.com>
2017-11-17 10:20:46 +1300

    Update
    Update on a Room Module.
    Navi-Bar

--- gallery.php
+++ gallery.php
@@ -16,7 +16,59 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <?php include "includes/navi_bar.php"; ?>
+     <nav class="navbar navbar-inverse ">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php"><img src="images/MRBSicon.png" width="100px" height="25px" alt="">
+          </a>
+
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul id="navibar" class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+              
+            <?php if (isset ($_SESSION["role"])) { 
+                
+    echo '
+            
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Booking<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="booking.php">Book room</a></li>';}
+                if (($_SESSION["role"] == "1")||($_SESSION["role"] == "0")){
+            echo '
+                <li><a href="booking_list.php">Booking list</a></li>'; }
+                if (isset ($_SESSION["role"])) { echo '                 
+                <li role="separator" class="divider"></li>
+                <li><a href="cancel_booking.php">Cancel booking</a></li>
+              </ul>
+            </li>'; } ?>
+            <?php if ($_SESSION["role"] == "0"){
+            echo '
+            '; } ?>
+            
+            <li><a href="room_list.php">Room List</a></li> 
+            <li><a href="contact.php">Contact us</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <?php if (isset($_SESSION["name"])) { echo '
+            <li><a href="userinfo.php?ID='.$_SESSION["login"].'">'.$_SESSION["name"].'</a></li>
+            <li><a href="index.php?logout=1"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>'; }
+            else {
+                echo '
+            <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Register</a></li>'; }?>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 	
  <div class="container">
 <div class="row">
@@ -32,9 +84,7 @@
     
         <?php 
             $ID=$_GET["ID"];
-            $conn = mysqli_connect("localhost", "root", "root");
-            //Selecting Database
-            $db = mysqli_select_db($conn, "MRBS");
+            include "includes/DB_connection.php";
             //sql query to fetch information of registerd user and finds user match.
             $query = mysqli_query($conn, "SELECT * FROM room");
             $rows = mysqli_num_rows($query);
@@ -162,10 +212,19 @@
     
 <?php include "includes/footer.php"; ?>
 
-    
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+     $('document').ready(function() {
+        $('.navbar-collapse li a').each(function() {
+            if ('http://localhost/ip_project/'+$(this).attr('href') == window.location.href)
+            {
+                $(this).parent().addClass('active');
+            }
+        });
+    }); 
+</script>
 </body>
 </html>
