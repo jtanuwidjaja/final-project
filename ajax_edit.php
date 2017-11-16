b61769e961d3f2d7617babe4ffb70e42f66a2b70
Konstantin <hvankostya@gmail.com>
2017-11-17 11:38:21 +1300

    Update Room module 11:38

--- ajax_edit.php
+++ ajax_edit.php
@@ -1,12 +1,13 @@
 <?php 
-include ("includes/DB_connection.php");
+$conn=mysqli_connect("localhost","root","root");
+mysqli_select_db($conn,"mrbs");
 $branch = isset($_GET['branch']) ? $_GET['branch'] : '';
 $tower = isset($_GET['tower']) ? $_GET['tower'] : '';
-
 
 if($branch!="")
 {
 $res=mysqli_query($conn, "SELECT * FROM building WHERE branchid=$branch");
+
 echo "<label for='ex2'>"; echo "Choose Tower*"; echo "</label>";
 echo "<select class='form-control' name='towerdd' id='towerdd' onchange='change_tower()'>";
 echo "<option>"; echo "Select"; echo "</option>";
@@ -14,9 +15,7 @@ while($row=mysqli_fetch_array($res))
 {
 echo "<option value='$row[buildingid]'>"; echo $row["buildingname"]; echo "</option>";
 }
-
 echo "</select>";
-
 }
 
 
@@ -25,7 +24,7 @@ if($tower!="")
 {
 $res=mysqli_query($conn, "SELECT * FROM building WHERE buildingid=$tower");
 echo "<label for='ex2'>"; echo "Choose Level*"; echo "</label>";
-echo "<select class='form-control' name='leveldd' id='level'>";
+echo "<select class='form-control' id='levelof' name='leveldd'>";
 echo "<option>"; echo "Select"; echo "</option>";
 
 $row=mysqli_fetch_array($res);
@@ -37,3 +36,4 @@ echo "<option value='$i'>".$i."</option>";
 echo "</select>";
 }
  ?>
+
